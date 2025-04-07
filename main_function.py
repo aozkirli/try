8737407d0091a69d6afd3169156d07f37c7d2fbb
aozkirli <88426801+aozkirli@users.noreply.github.com>
2025-04-07 15:24:18 +0200

    Update main_function.py

--- main_function.py
+++ main_function.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import process_combine_experiments
 import process_create_master_table
-%run process_combine_experiments.py
-%run process_create_master_table.py
+process_combine_experiments.py
+process_create_master_table.py
 
